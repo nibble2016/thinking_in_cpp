02077ef5e1d9e46414ed7632f07a2676954b2d55
gwq <gwq5210@qq.com>
2014-11-05 21:11:52 +0800

    add file 2.4.1_concat.cpp

---
+++ ch02/2.4.1_concat.cpp
@@ -0,0 +1,50 @@
+/*************************************************************************
+	> File Name: 2.4.1_concat.cpp
+	> Author: gwq
+	> Mail: gwq5210@qq.com 
+	> Created Time: 2014年11月05日 星期三 21时06分45秒
+ ************************************************************************/
+
+#include <cmath>
+#include <ctime>
+#include <cctype>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+#define INF (INT_MAX / 10)
+#define clr(arr, val) memset(arr, val, sizeof(arr))
+#define pb push_back
+#define sz(a) ((int)(a).size())
+
+using namespace std;
+typedef set<int> si;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef long long ll;
+
+const double esp = 1e-5;
+
+//可以实现字符数组的拼接（character array concatenation）
+//如果两个加引号的字符数组邻接，并且它们之间没有任何标点，
+//编译器就会把这些字符数组连接成单个字符数组。
+int main(int argc, char *argv[])
+{
+	cout << "This is far too long to put on a "
+		"single line but it can be broken up with "
+		"no ill effects\nas long as there is no "
+		"punctuation separating adjacent character "
+		"arrays.\n";
+
+	return 0;
+}
