501ee1e96112eb277b2fcc8fc9cac0490ee31775
gwq <gwq5210@qq.com>
2015-01-21 11:22:35 +0800

    new file const.c const.cpp

---
+++ ch08/const.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: const.c
+	> Author: gwq
+	> Mail: gwq5210@qq.com 
+	> Created Time: 2015年01月21日 星期三 11时20分39秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	const int a = 10;
+	// c语言中不能这样写
+	//int b[a] = {0};
+	printf("%lu\n", sizeof(b));
+	printf("%d\n", a);
+	return 0;
+}
---
+++ ch08/const.cpp
@@ -0,0 +1,45 @@
+/*************************************************************************
+	> File Name: const.cpp
+	> Author: gwq
+	> Mail: gwq5210@qq.com 
+	> Created Time: 2015年01月21日 星期三 11时20分11秒
+ ************************************************************************/
+
+#include <cmath>
+#include <ctime>
+#include <cctype>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+#define INF (INT_MAX / 10)
+#define clr(arr, val) memset(arr, val, sizeof(arr))
+#define pb push_back
+#define sz(a) ((int)(a).size())
+
+using namespace std;
+typedef set<int> si;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef long long ll;
+
+const double esp = 1e-5;
+
+int main(int argc, char *argv[])
+{
+	const int a = 10;
+	int b[a] = {0};
+	cout << sizeof(b) << endl;
+	return 0;
+}
