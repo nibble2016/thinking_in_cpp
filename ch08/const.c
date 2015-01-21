5409c8295509c2da53430f2748e2cbb0c5071074
gwq <gwq5210@qq.com>
2015-01-21 13:11:02 +0800

    change file const.c

--- ch08/const.c
+++ ch08/const.c
@@ -10,9 +10,12 @@
 int main(int argc, char *argv[])
 {
 	const int a = 10;
+	int c = 10;
 	// c语言中不能这样写
 	//int b[a] = {0};
-	printf("%lu\n", sizeof(b));
+	//printf("%lu\n", sizeof(b));
 	printf("%d\n", a);
+	const int b = a + c;
+	printf("%d\n", b);
 	return 0;
 }
